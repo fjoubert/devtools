6fcbe6ca516ce80856a7682a494c1a54ca18c6a7
Francois <francoisjj@gmail.com>
2021-01-01 20:24:10 +0000

    Time tool refinement

--- src/tools/time.tsx
+++ src/tools/time.tsx
@@ -1,95 +1,75 @@
 import { ChangeEvent, useEffect, useState } from 'react';
-import { DateTime, Duration } from 'luxon';
+import { DateTime, Duration, DurationObjectUnits } from 'luxon';
 import HumanizeDuration from 'humanize-duration';
 import withErrorHandling, { ErrorHandlerProps } from '../util/withErrorHandingWrapper';
 
-interface ITimestampsProps {
-    timestamp: DateTime,
-    unixTimestamp: number,
-    isoTimestamp: string,
-    RFC2822: string,
-    localeString: string,
+interface ITimestampProps {
+    dateTime: DateTime,
+    unixTimestamp?: number,
+    isoTimestamp?: string,
+    RFC2822?: string,
     formattedString: string,
 }
 
-interface IDurationsProps {
-    seconds: number,
+interface IDurationProps {
+    values: Duration,
     formattedString: string,
     humanized: string,
 }
 
 const maxTimestamp = 999999999999;
 
+const defaultDuration = Duration.fromObject(
+    {
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 300,
+        milliseconds: 0,
+    }
+);
+
 const Time = (errorHandlerProps: ErrorHandlerProps) => {
 
-    const [timeFormat, setTimeFormat] = useState<string>("yyyy-MM-dd HH:mm:ss.SSS");
+    const [timeFormat, setTimeFormat] = useState<string>("yyyy-MM-dd HH:mm:ss");
     const [durationFormat, setDurationFormat] = useState<string>("h:mm:ss");
 
-    const [timestamps, setTimestamps] = useState<ITimestampsProps>({
-        timestamp: DateTime.utc(),
+    const [timestamp, setTimestamp] = useState<ITimestampProps>({
+        dateTime: DateTime.utc(),
         unixTimestamp: 0,
         isoTimestamp: '',
         RFC2822: '',
-        localeString: '',
         formattedString: '',
     });
-    const [durations, setDurations] = useState<IDurationsProps>({
-        seconds: 0,
+
+    const [duration, setDuration] = useState<IDurationProps>({
+        values: defaultDuration,
         formattedString: '',
         humanized: '',
     });
 
     useEffect(() => {
-        setTimestamps(computeTimestamps(DateTime.utc().toSeconds(), timeFormat));
-        setDurations(computeDurations(7201, durationFormat));
+        setTimestamp(computeTimestamp(DateTime.utc(), timeFormat));
+        setDuration(computeDuration(defaultDuration, durationFormat));
     }, [durationFormat, timeFormat]);
 
     const setNow = () => {
-        setTimestamps(computeTimestamps(DateTime.utc().toSeconds(), timeFormat));
+        setTimestamp(computeTimestamp(DateTime.utc(), timeFormat));
     };
 
-    const handleUnixInputChange = withErrorHandling((e: ChangeEvent<HTMLInputElement>) => {
-        setTimestamps(computeTimestamps(parseInt(e.target.value), timeFormat));
-    }, errorHandlerProps);
-
-    const handleTimeISOInputChange = withErrorHandling((e: ChangeEvent<HTMLInputElement>) => {
-        let newDateTime = DateTime.fromISO(e.target.value);
-        if (!newDateTime.isValid) {
-            setTimestamps({
-                ...timestamps,
-                isoTimestamp: e.target.value
-            });
-            throw new Error(newDateTime.invalidExplanation!);
+    const setTimestampFromDateTime = withErrorHandling((dateTime: DateTime) => {
+        if (!dateTime.isValid) {
+            throw new Error(dateTime.invalidExplanation!);
         }
-        setTimestamps(computeTimestamps(newDateTime.toSeconds(), timeFormat));
+        setTimestamp(computeTimestamp(dateTime, timeFormat));
     }, errorHandlerProps);
 
-    const handleRFC2822InputChange = withErrorHandling((e: ChangeEvent<HTMLInputElement>) => {
-        let newDateTime = DateTime.fromRFC2822(e.target.value);
-        if (!newDateTime.isValid) {
-            setTimestamps({
-                ...timestamps,
-                RFC2822: e.target.value
-            });
-            throw new Error(newDateTime.invalidExplanation!);
-        }
-        setTimestamps(computeTimestamps(newDateTime.toSeconds(), timeFormat));
-    }, errorHandlerProps);
-
-    const handleTimeFormattedInputChange = withErrorHandling((e: ChangeEvent<HTMLInputElement>) => {
-        let newDateTime = DateTime.fromFormat(e.target.value, timeFormat);
-        if (!newDateTime.isValid) {
-            setTimestamps({
-                ...timestamps,
-                formattedString: e.target.value
-            });
-            throw new Error(newDateTime.invalidExplanation!);
-        }
-        setTimestamps(computeTimestamps(newDateTime.toSeconds(), timeFormat));
-    }, errorHandlerProps);
-
-    const handleDurationSecondsInputChange = withErrorHandling((e: ChangeEvent<HTMLInputElement>) => {
-        setDurations(computeDurations(parseInt(e.target.value), durationFormat));
+    const setDurationFromDurationObject = withErrorHandling((durationSegment: DurationObjectUnits) => {
+        duration.values.set(durationSegment);
+        setDuration(computeDuration(duration.values, durationFormat));
     }, errorHandlerProps);
 
     const handleFocus = (e: ChangeEvent<HTMLInputElement>) => e.target.select();
@@ -99,124 +79,160 @@ const Time = (errorHandlerProps: ErrorHandlerProps) => {
         <div className="timeTool">
             <span className="divider">Time<button onClick={setNow}>Now</button></span>
             <div className="timeRow">
-                <label htmlFor="inputUnix">Unix (seconds)</label>
+                <label>Unix (seconds)</label>
                 <input
-                    id="inputUnix"
                     autoFocus
                     type="text"
                     min="0"
                     max={maxTimestamp}
-                    value={timestamps.unixTimestamp}
-                    onChange={handleUnixInputChange}
+                    value={timestamp.unixTimestamp}
+                    onChange={(e) => { setTimestampFromDateTime(DateTime.fromSeconds(parseInt(e.target.value))); }}
                 />
             </div>
             <div className="timeRow">
-                <label htmlFor="inputFormat">Format</label>
+                <label>Format</label>
                 <a href="https://moment.github.io/luxon/docs/manual/parsing.html#table-of-tokens" target="_blank" rel="noreferrer">?</a>
                 <input
-                    id="inputFormat"
                     type="text"
                     value={timeFormat}
                     onChange={(e) => setTimeFormat(e.target.value)}
                 />
             </div>
             <div className="timeRow">
-                <label htmlFor="inputFormattedString">Formatted String</label>
+                <label>Formatted String</label>
                 <input
-                    id="inputFormattedString"
                     type="text"
-                    value={timestamps.formattedString}
-                    onChange={handleTimeFormattedInputChange}
+                    value={timestamp.formattedString}
+                    onChange={(e) => { setTimestampFromDateTime(DateTime.fromFormat(e.target.value, timeFormat)); }}
                     onFocus={handleFocus}
                 />
             </div>
             <div className="timeRow">
-                <label htmlFor="inputISO">ISO 8601</label>
+                <label>ISO 8601</label>
                 <input
-                    id="inputISO"
                     type="text"
-                    value={timestamps.isoTimestamp}
-                    onChange={handleTimeISOInputChange}
-                    onFocus={handleFocus} />
+                    value={timestamp.isoTimestamp}
+                    onChange={(e) => { setTimestampFromDateTime(DateTime.fromISO(e.target.value)); }}
+                    onFocus={handleFocus}
+                />
             </div>
             <div className="timeRow">
-                <label htmlFor="inputRFC2822">RFC2822</label>
+                <label>RFC2822</label>
                 <input
-                    id="inputRFC2822"
                     type="text"
-                    value={timestamps.RFC2822}
-                    onChange={handleRFC2822InputChange}
+                    value={timestamp.RFC2822}
+                    onChange={(e) => { setTimestampFromDateTime(DateTime.fromRFC2822(e.target.value)); }}
                     onFocus={handleFocus}
                 />
             </div>
             <span className="divider">Duration</span>
             <div className="timeRow">
-                <label htmlFor="inputDurationSeconds">Duration (seconds)</label>
+                <label>Years</label>
                 <input
-                    id="inputDurationSeconds"
-                    type="text"
-                    value={durations!.seconds}
-                    onChange={handleDurationSecondsInputChange}
+                    type="number"
+                    value={duration.values.years}
+                    onChange={(e) => { setDurationFromDurationObject({ years: parseInt(e.target.value) }); }}
                     onFocus={handleFocus}
                 />
             </div>
             <div className="timeRow">
-                <label htmlFor="inputDurationFormat">Format</label>
+                <label>Months</label>
+                <input
+                    type="number"
+                    value={duration.values.months}
+                    onChange={(e) => { setDurationFromDurationObject({ months: parseInt(e.target.value) }); }}
+                    onFocus={handleFocus}
+                />
+            </div>
+            <div className="timeRow">
+                <label>Weeks</label>
+                <input
+                    type="number"
+                    value={duration.values.weeks}
+                    onChange={(e) => { setDurationFromDurationObject({ weeks: parseInt(e.target.value) }); }}
+                    onFocus={handleFocus}
+                />
+            </div>
+            <div className="timeRow">
+                <label>Days</label>
+                <input
+                    type="number"
+                    value={duration.values.days}
+                    onChange={(e) => { setDurationFromDurationObject({ days: parseInt(e.target.value) }); }}
+                    onFocus={handleFocus}
+                />
+            </div>
+            <div className="timeRow">
+                <label>Hours</label>
+                <input
+                    type="number"
+                    value={duration.values.hours}
+                    onChange={(e) => { setDurationFromDurationObject({ hours: parseInt(e.target.value) }); }}
+                    onFocus={handleFocus}
+                />
+            </div>
+            <div className="timeRow">
+                <label>Minutes</label>
+                <input
+                    type="number"
+                    value={duration.values.minutes}
+                    onChange={(e) => { setDurationFromDurationObject({ minutes: parseInt(e.target.value) }); }}
+                    onFocus={handleFocus}
+                />
+            </div>
+            <div className="timeRow">
+                <label>Seconds</label>
+                <input
+                    type="number"
+                    value={duration.values.seconds}
+                    onChange={(e) => { setDurationFromDurationObject({ seconds: parseInt(e.target.value) }); }}
+                    onFocus={handleFocus}
+                />
+            </div>
+            <div className="timeRow">
+                <label>Format</label>
                 <a href="https://moment.github.io/luxon/docs/manual/parsing.html#table-of-tokens" target="_blank" rel="noreferrer">?</a>
                 <input
-                    id="inputDurationFormat"
                     type="text"
                     value={durationFormat}
                     onChange={(e) => setDurationFormat(e.target.value)}
                 />
             </div>
             <div className="timeRow">
-                <label htmlFor="inputFormattedDurationString">Formatted String</label>
+                <label>Formatted String</label>
                 <input
-                    id="inputFormattedDurationString"
                     type="text"
-                    defaultValue={durations!.formattedString}
+                    defaultValue={duration.formattedString}
                     onFocus={handleFocus}
                 />
             </div>
 
-            <label className="divider" htmlFor="inputHumanized">Humanized</label>
+            <label className="divider">Humanized</label>
             <textarea
                 id="inputHumanized"
-                defaultValue={durations!.humanized}
+                defaultValue={duration.humanized}
                 onFocus={handleTextareaFocus}
             />
         </div>
     );
 };
 
-function computeDurations(seconds: number, durationFormat: string) {
-    let newSeconds = seconds;
-    if (isNaN(seconds)) {
-        newSeconds = 0;
-    }
+export const computeDuration = (duration: Duration, durationFormat: string): IDurationProps => {
     return {
-        seconds: seconds,
-        formattedString: Duration.fromMillis(newSeconds * 1000).toFormat(durationFormat),
-        humanized: HumanizeDuration(newSeconds * 1000)
+        values: duration,
+        formattedString: duration.toFormat(durationFormat),
+        humanized: HumanizeDuration(duration.as('milliseconds'))
     };
 };
 
-const computeTimestamps = (seconds: number, timeFormat: string) => {
-    let newSeconds = seconds;
-    if (isNaN(seconds)) {
-        newSeconds = 0;
-    } else if (seconds > maxTimestamp) {
-        newSeconds = maxTimestamp;
-    }
-    let timestamp = DateTime.fromSeconds(newSeconds);
+export const computeTimestamp = (dateTime: DateTime, timeFormat: string): ITimestampProps => {
+
     return {
-        timestamp: timestamp,
-        unixTimestamp: newSeconds,
-        isoTimestamp: timestamp.toISO(),
-        RFC2822: timestamp.toRFC2822(),
-        localeString: timestamp.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS),
-        formattedString: timestamp.toFormat(timeFormat)
+        dateTime: dateTime,
+        unixTimestamp: dateTime.toSeconds(),
+        isoTimestamp: dateTime.toISO(),
+        RFC2822: dateTime.toRFC2822(),
+        formattedString: dateTime.toFormat(timeFormat)
     };
 };
 
